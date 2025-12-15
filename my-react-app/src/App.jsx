cf6b317169c9fffebdeb40efcd027e4bb04b371a
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2025-12-15 13:53:46 +0530

    Add files via upload

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Feedback from "./components/Feedback";
 import AdminDashboard from "./components/AdminDashboard";
-// function AdminDashboard() {
-//   return (
-//     <div className="min-h-screen bg-slate-900 text-white p-10">
-//       <h1 className="text-4xl font-bold">ADMIN PAGE WORKING</h1>
-//       <p>If you see this, routing is correct.</p>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
